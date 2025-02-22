f477e542792ff38b116dc93f42340ad27ff3dd46
Bibek Panthi <bpanthi977@gmail.com>
2025-02-21 18:18:47 -0600

    cua and backup files

--- init.el
+++ init.el
@@ -3,8 +3,15 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
-(defmacro load* (path)
-  `(load (expand-file-name ,path (file-name-directory load-file-name))))
+;; Basic config
+(setf init-dir (file-name-directory load-file-name))
 
 (setf custom-file "custom.el")
+(cua-mode t)
+(setf backup-directory-alist `(("." . ,(concat init-dir "/savefiles/backups/"))))
+
+;; Load modules
+(defun load* (path)
+  (load (expand-file-name path (file-name-directory load-file-name))))
+
 (load* "modules/init.el")
