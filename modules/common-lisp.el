16ee1d4fd43a8b4026bc9eed8498f736b2796a07
Bibek Panthi <bpanthi977@gmail.com>
2025-03-05 23:46:31 -0600

    sly-mrepl C-c M-p

--- modules/common-lisp.el
+++ modules/common-lisp.el
@@ -2,3 +2,8 @@
   :ensure t
   :config
   (setf inferior-lisp-program "sbcl"))
+
+(use-package sly-mrepl
+  :ensure nil
+  :bind (:map sly-mrepl-mode-map
+	      ("C-c M-p" . sly-mrepl-set-package)))
