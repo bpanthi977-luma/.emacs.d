520c3cf01215766f615c6809e0d01bf45ff79f20
Bibek Panthi <bpanthi977@gmail.com>
2025-03-10 14:32:42 -0500

    Add which-key

--- modules/keymap.el
+++ modules/keymap.el
@@ -2,6 +2,11 @@
   :ensure t
   :demand t)
 
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode t))
+
 (defvar bp/global-prefix "M-m")
 (define-prefix-command 'bp/global-prefix-map)
 (define-key global-map (kbd bp/global-prefix) 'bp/global-prefix-map)
