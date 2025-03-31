5ad28a3801d3770d1b1c67da85d90b8a866e5e70
Bibek Panthi <bpanthi977@gmail.com>
2025-03-31 01:39:55 -0500

    project save-all-buffers C-x p C-s

--- modules/project.el
+++ modules/project.el
@@ -1,7 +1,24 @@
 (use-package project
+  :bind (("C-x p C-s" . bp/project-save-all-buffers))
   :config
   (defun bp/projectile-dir-p (dir)
     (let ((root (locate-dominating-file dir ".projectile")))
       (when root
 	(cons 'transient root))))
+
+  (defun bp/project-save-all-buffers ()
+    "Save all buffers that belong to the current project."
+    (interactive)
+    (let* ((project (project-current))
+	   (buffers (when project (project-buffers project))))
+      (if buffers
+	  (progn
+	    (dolist (buf buffers)
+	      (when (buffer-file-name buf)
+		(with-current-buffer buf
+		  (when (buffer-modified-p)
+		    (save-buffer)))))
+	    (message "All project buffers saved."))
+	(message "No project found or no buffers to save."))))
+
   (add-to-list 'project-find-functions #'bp/projectile-dir-p))
