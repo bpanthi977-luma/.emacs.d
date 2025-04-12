21b97cb69bdb38875f40d85d5dff83134850c3b4
Bibek Panthi <bpanthi977@gmail.com>
2025-04-12 14:23:14 -0500

    Remove org-capture clock
    Many mispresses happend because the shortcut is 'c'

--- modules/org/org-capture.el
+++ modules/org/org-capture.el
@@ -4,7 +4,7 @@
   :init
   (setq org-capture-templates `(("t" "Todo" entry (file+datetree "~/org/private/tasks.org")
 				 "* TODO %?\nCREATED: %U\n %i\n  %a")
-                                ("c" "Clock" entry (file+datetree "~/org/private/tasks.org")
+                                ("l" "Log Time" entry (file+datetree "~/org/private/tasks.org")
                                  "* %? \n" :clock-in t :clock-keep t)
                                 ("j" "Journal" entry (file+datetree "~/org/private/journal.org.gpg")
                                  "* %?\nEntered on %U\n  %i\n  %a")
