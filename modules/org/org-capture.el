2a0ce41f202161fcd3a457c7615bf3d83c8b08fd
Bibek Panthi <bpanthi977@gmail.com>
2025-07-12 10:38:12 -0500

    Add snippet to org-capture-templates

--- modules/org/org-capture.el
+++ modules/org/org-capture.el
@@ -3,7 +3,7 @@
 	      ("o c" . org-capture))
   :init
   (setq org-capture-templates `(("t" "Todo" entry (file+datetree "~/org/private/tasks.org")
-				 "* TODO %?\nCREATED: %U\n %i\n  %a")
+				 "* TODO %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n %i\n  %a")
                                 ("l" "Log Time" entry (file+datetree "~/org/private/tasks.org")
                                  "* %? \n" :clock-in t :clock-keep t :clock-resume t)
                                 ("j" "Journal" entry (file+datetree "~/org/private/journal.org.gpg")
@@ -12,6 +12,9 @@
                                  "* %?\n %i \n %a \n"
                                  :time-prompt t)
                                 ("n" "Note" entry (file "~/org/private/notes.org" )
-                                 "* %?\nCREATED: %U\n")
+                                 "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n")
+				("s" "Snippet" entry (file "~/org/snippets.org")
+                                 "* %?\n:PROPERTIES:\n:CREATED: %U\n:END:\n"
+				 :hook org-id-get-create)
                                 ("k" "Quote" item (file+headline "~/org/private/notes.org" "Quotes")
                                  "%? :: %x"))))
