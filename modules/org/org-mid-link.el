5c81a67489bc0649703d7af41add91c15dd591a2
Bibek Panthi <bpanthi977@gmail.com>
2025-08-27 17:03:02 -0500

    Support imap-message: links in org-mid-links
    Drag and drop on thunderbird emails creates a imap-message: link

--- modules/org/org-mid-link.el
+++ modules/org/org-mid-link.el
@@ -8,13 +8,20 @@
 		:command (list org-mid-link-email-program
 			       (concat "mid:" path))))
 
+(defun org-imap-message-follow (path &optional arg)
+  "Open the email `PATH'"
+  (make-process :name "thunderbird"
+		:command (list org-mid-link-email-program
+			       (concat "imap-message:" path))))
+
 (defun org-mid-link-insert (message-id)
   (interactive "sMessage Id: ")
   (let ((stripped (string-trim message-id "<" ">")))
     (insert "[[mid:" stripped "]]")))
 
 (defun org-mid-link-setup ()
-  (org-link-set-parameters "mid" :follow #'org-mid-link-follow))
+  (org-link-set-parameters "mid" :follow #'org-mid-link-follow)
+  (org-link-set-parameters "imap-message" :follow #'org-imap-message-follow))
 
 (with-eval-after-load 'org
   (org-mid-link-setup))
