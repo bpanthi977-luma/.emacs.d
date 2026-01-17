f40d4462ca9399e02e1263762c63da3df41b5816
Bibek Panthi <bpanthi977@gmail.com>
2026-01-17 00:11:33 -0600

    bp/eglot-config-python

--- modules/lsp.el
+++ modules/lsp.el
@@ -2,7 +2,17 @@
   :ensure t
   :defer t
   :config
-  (setf eglot-autoshutdown t))
+  (setf eglot-autoshutdown t)
+
+  (defun bp/eglot-config-python (venv-dir)
+    "Takes the directory of virtualenv as argument and configures eglot"
+    (interactive "DSelect Virtualenv Directory: ")
+    (add-dir-local-variable 'python-mode
+			    'eglot-workspace-configuration
+			    `((:python :venvPath ,(file-relative-name venv-dir default-directory)
+				       :pythonPath ,(file-relative-name
+						     (expand-file-name "./bin/python" venv-dir)
+						     default-directory))))))
 
 (use-package dap-mode
   :ensure t
