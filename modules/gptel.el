2e89a2223bcdabdae84e32fbf4a3067636ae2662
Bibek Panthi <bpanthi977@gmail.com>
2026-01-19 20:24:35 -0600

    Add OpenRouter to gptel

--- modules/gptel.el
+++ modules/gptel.el
@@ -12,4 +12,18 @@
   ;; machine generativelanguage.googleapis.com apikey password <<YOUR_API_KEY>>
   (setf gptel-default-mode 'org-mode
 	gptel-model 'gemini-flash-latest
-	gptel-backend (gptel-make-gemini "Gemini" :stream t :key #'gptel-api-key-from-auth-source)))
+	gptel-backend (gptel-make-gemini "Gemini" :stream t :key #'gptel-api-key-from-auth-source))
+
+  (gptel-make-openai "OpenRouter" ;; Any name you like
+    :host "openrouter.ai"
+    :endpoint "/api/v1/chat/completions"
+    :protocol "https"
+    :stream t
+    :key #'gptel-api-key-from-auth-source
+    :models '("openrouter/mistral-7b" ;; example models
+	      "openrouter/gpt-4.1-mini"
+	      "qwen/qwen3-4b:free"
+	      "mistralai/mistral-small-3.1-24b-instruct:free"
+	      "google/gemma-3-27b-it:free"
+	      "meta-llama/llama-3.3-70b-instruct:free"
+	      "meta-llama/llama-3.1-405b-instruct:free")))
