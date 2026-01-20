087c9b9ed7a3ee264e503c25c230e50078e38635
Bibek Panthi <bpanthi977@gmail.com>
2026-01-19 20:24:46 -0600

    Add plan.org to org-agenda-files

--- modules/org/agenda.el
+++ modules/org/agenda.el
@@ -6,5 +6,5 @@
   (define-key org-agenda-mode-map (kbd bp/global-prefix) bp/global-prefix-map)
 
   (setf org-agenda-span 'day
-	org-agenda-files '("private/tasks.org" "private/tasks.org_archive")
+	org-agenda-files '("private/tasks.org" "private/tasks.org_archive" "private/plan.org")
 	org-agenda-skip-scheduled-if-deadline-is-shown t))
