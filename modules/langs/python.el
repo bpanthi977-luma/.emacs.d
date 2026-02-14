107aac10eb3bec3026ace518ed3f19c9ef3b6e8f
Bibek Panthi <bpanthi977@gmail.com>
2026-02-13 19:12:47 -0600

    python.el: bp/uv-setup-venv-location

--- modules/langs/python.el
+++ modules/langs/python.el
@@ -1,2 +1,18 @@
 (use-package uv-mode
-  :ensure t)
+  :ensure t
+  :config
+
+  (defun bp/uv-setup-venv-location (dir)
+    "Create .venv configuration file in DIR to set up the uv environment."
+    (interactive "DDirectory: ")
+    (let* ((expanded-dir (expand-file-name dir))
+	   (content (format "
+export UV_PROJECT_ENVIRONMENT=%s
+
+if [ -f \"${UV_PROJECT_ENVIRONMENT}/bin/activate\" ]; then
+    source \"${UV_PROJECT_ENVIRONMENT}/bin/activate\"
+fi"
+			    expanded-dir)))
+      (with-temp-file ".venv"
+	(insert content))
+      (message "Successfully wrote environment setup to .venv" ))))
