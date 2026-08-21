dea415cbc71dd6071bc4c96329185109dae9c34b
Bibek Panthi <bpanthi977@gmail.com>
2026-08-20 20:01:33 -0700

    fix attach dir name for .org.gpg files

--- modules/org/org-attach.el
+++ modules/org/org-attach.el
@@ -8,12 +8,19 @@
 	org-attach-id-dir "data/"
 	org-attach-use-inheritance t)
 
+  (defun bp/file-name-nonextension (filename)
+    (let ((parts (string-split filename "\\.")))
+      (if (or (null parts)
+	      (string-empty-p (first parts)))
+	  filename
+	(first parts))))
+
   (defun bp/org-attach-dir (id)
     "Function to be used as member of `org-attach-id-to-path-function-list'
 Takes `id' of the node, and returns the attach directory.
 Returns NIL if fallback to other id to path functions is wanted."
     (let* ((filename (buffer-file-name (or (buffer-base-buffer) (current-buffer))))
-	   (basename (and filename (file-name-base filename))))
+	   (basename (and filename (bp/file-name-nonextension (file-name-nondirectory filename)))))
       basename))
 
   (cl-pushnew #'bp/org-attach-dir org-attach-id-to-path-function-list))
